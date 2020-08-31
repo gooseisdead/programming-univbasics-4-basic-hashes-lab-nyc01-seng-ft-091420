bd7b613e572dcc8155969d9cdf008e86fba308fa
Mike Smith <gooseisdead@mac.com>
2020-08-31 02:33:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,12 +3,12 @@ def new_hash
 end
 
 def my_hash
-  costumes = { "captain" => "Shield", "ironman" => "Helmet", "thor" => "Hammer" }
+  costumes = { captain => "Shield", ironman => "Helmet", thor => "Hammer" }
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-end
+  
+  end
 
 
 def id_hash_generator(number)
