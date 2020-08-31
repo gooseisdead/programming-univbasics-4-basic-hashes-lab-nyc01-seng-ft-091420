735378c7428291e6d500c4ba8950223e40122f29
Mike Smith <gooseisdead@mac.com>
2020-08-31 02:28:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,7 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  costumes = { "captain" => "Shield", "ironman" => "Helmet", "thor" => "Hammer" }
 end
 
 def pioneer
